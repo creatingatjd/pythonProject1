d0ea916ef148f1e93b9ae03a6b5902bf5b4d0d0d
creatingatjd <126667597+creatingatjd@users.noreply.github.com>
2024-06-14 18:30:59 +0100

    Update settings.py

--- webpro/settings.py
+++ webpro/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-#(!lc2%nj#_wd6jh(cl#bx0iu7&tfm^yrcr&o3z3-ai-x$n&co
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0']
+ALLOWED_HOSTS = ['localhost:8002']
 
 
 # Application definition
